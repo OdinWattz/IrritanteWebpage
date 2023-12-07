8abaacc8b1e3c40dfe6889b7332533328b631bac
Odin Wattez <145436065+OdinWattz@users.noreply.github.com>
2023-12-07 14:15:57 +0100

    java.js

--- irretant_webpagina/java/java.js
+++ irretant_webpagina/java/java.js
@@ -43,3 +43,8 @@ if(i = 1){
 apple.addEventListener('click', cloneAndPositionButton);
 
 }
+
+function playMusic(){
+    var music = new Audio("./audio/460432__jay_you__music-elevator.wav");
+    music.play();
+    }
