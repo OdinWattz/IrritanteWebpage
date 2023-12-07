528fb9f483752fd24f35517cb45f4df2dd1c3b62
Mohamad-mourkab <145436414+Mohamad-mourkab@users.noreply.github.com>
2023-12-07 13:17:21 +0100

    comment weg halen

--- irretant_webpagina/java/java.js
+++ irretant_webpagina/java/java.js
@@ -2,8 +2,8 @@ function getRandomPosition() {
     let screenWidth = window.innerWidth;
     let screenHeight = window.innerHeight;
 
-    let randomX = Math.floor(Math.random() * (screenWidth - 128)); // Adjust 100 as per your button width
-    let randomY = Math.floor(Math.random() * (screenHeight - 43)); // Adjust 40 as per your button height
+    let randomX = Math.floor(Math.random() * (screenWidth - 128)); 
+    let randomY = Math.floor(Math.random() * (screenHeight - 43));
 
     return { x: randomX, y: randomY };
 }
@@ -12,23 +12,19 @@ let section = document.getElementById('section')
 let apple = document.getElementById('appleIcon')
 
 function cloneAndPositionButton() {
-    // Clone the button
 
     let clonedButton = apple.cloneNode(true);
 
-    // Optionally, change the ID or any other attributes of the cloned button
+
     clonedButton.id = 'clonedButton';
 
-    // Set random position
     let randomPosition = getRandomPosition();
     clonedButton.style.left = randomPosition.x + 'px';
     clonedButton.style.top = randomPosition.y + 'px';
 
-    // Add the cloned button to the body
     document.body.appendChild(clonedButton);
 }
 
-// Call the function to clone and position the button when the original button is clicked
 
 function iphone(){
 let i = 1;
