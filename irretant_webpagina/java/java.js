6c7f36d5bac58d5e06a9c8e5c33fe2777e987b45
Mohamad-mourkab <145436414+Mohamad-mourkab@users.noreply.github.com>
2023-12-21 10:46:25 +0100

    improvements

--- irretant_webpagina/java/java.js
+++ irretant_webpagina/java/java.js
@@ -1,11 +1,11 @@
 function getRandomPosition() {
-    let screenWidth = window.innerWidth;
-    let screenHeight = window.innerHeight;
+  let screenWidth = window.innerWidth;
+  let screenHeight = window.innerHeight;
 
-    let randomX = Math.floor(Math.random() * (screenWidth - 127)); 
-    let randomY = Math.floor(Math.random() * (screenHeight - 42));
+  let randomX = Math.floor(Math.random() * (screenWidth - 127));
+  let randomY = Math.floor(Math.random() * (screenHeight - 42));
 
-    return { x: randomX, y: randomY };
+  return { x: randomX, y: randomY };
 }
 
 let section = document.getElementById('section')
@@ -13,48 +13,48 @@ let apple = document.getElementById('appleIcon')
 
 function cloneAndPositionButton() {
 
-    let clonedButton = apple.cloneNode(true);
+  let clonedButton = apple.cloneNode(true);
 
 
-    clonedButton.id = 'clonedButton';
+  clonedButton.id = 'clonedButton';
 
-    let randomPosition = getRandomPosition();
-    clonedButton.style.left = randomPosition.x + 'px';
-    clonedButton.style.top = randomPosition.y + 'px';
+  let randomPosition = getRandomPosition();
+  clonedButton.style.left = randomPosition.x + 'px';
+  clonedButton.style.top = randomPosition.y + 'px';
 
-    document.body.appendChild(clonedButton);
+  document.body.appendChild(clonedButton);
 }
 
 
-function backgroundChange(){
-let i = 1;
+function backgroundChange() {
+  let i = 1;
 
-if(i = 1){
-    section.style.backgroundColor = 'yellow';
-    i++;
-}else if(i = 2){
-    section.style.backgroundColor = 'green';
-    i++
-}else{
-    section.style.backgroundColor = 'white';
-    i = 0
+  if (i = 1) {
+      section.style.backgroundColor = 'yellow';
+      i++;
+  } else if (i = 2) {
+      section.style.backgroundColor = 'green';
+      i++
+  } else {
+      section.style.backgroundColor = 'white';
+      i = 0
+  }
+
+  apple.addEventListener('click', cloneAndPositionButton);
+
 }
 
-apple.addEventListener('click', cloneAndPositionButton);
-
+function playMusic() {
+  var music = new Audio("../audio/460432__jay_you__music-elevator.wav");
+  music.play();
 }
-
-function playMusic(){
-    var music = new Audio("../audio/460432__jay_you__music-elevator.wav");
-    music.play();
-    }
 
 
 let popUp = document.getElementById("cookiePopup");
 
 //Wanneer er dus op accept geklikt wordt, krijg je een cookie in je browser
 document.getElementById("acceptCookie").addEventListener("click", () => {
-// Hoelang de cookie geldig is: 5 minuten
+  // Hoelang de cookie geldig is: 5 minuten
   let d = new Date();
   d.setMinutes(5 + d.getMinutes());
 
@@ -73,13 +73,13 @@ const checkCookie = () => {
   let input = document.cookie.split("=");
   //Check voor de cookie
   if (input[0] == "myCookieName") {
-    //Hide the popup
-    popUp.classList.add("hide");
-    popUp.classList.remove("show");
+      //Hide the popup
+      popUp.classList.add("hide");
+      popUp.classList.remove("show");
   } else {
-    //Show the popup
-    popUp.classList.add("show");
-    popUp.classList.remove("hide");
+      //Show the popup
+      popUp.classList.add("show");
+      popUp.classList.remove("hide");
   }
 };
 
@@ -87,6 +87,6 @@ const checkCookie = () => {
 //Checkt of de cookie al bestaat wanneer de pagina laad
 window.onload = () => {
   setTimeout(() => {
-    checkCookie();
+      checkCookie();
   }, 2000);
 };
